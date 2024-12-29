4435a96eafaeffc92b129364b6726a1fa42a11e4
Harshith C <140620151+ItachiPrime@users.noreply.github.com>
2024-12-29 20:27:35 +0530

    Update firebase.config.ts

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAoF8lBhCLzs4obIVLkaSTrVUAZ0wfUiF0",
@@ -12,4 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+export { db };
 export { auth };
